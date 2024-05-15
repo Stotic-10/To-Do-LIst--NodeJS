b8ba18c58d3dc5c0d2d305fcc423dda3f86a7de2
Stotic-10 <webdevloper654@gmail.com>
2024-05-15 08:55:10 +0530

    update

---
+++ server.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const app = express();
+
+const port = 3000;
+let id = 1;
+
+let data = [
+  {
+    id: 1,
+    task: "Task-1",
+    dsc: "Developing App",
+    date: "2024-03-23",
+  },
+];
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  return res.render("form");
+});
+
+app.post("/insert", (req, res) => {
+  let AddData = {
+    id: id++,
+    task: req.body.task,
+    dsc: req.body.dsc,
+    date: req.body.date,
+  };
+
+  data.push(AddData);
+  console.log(AddData);
+  return res.redirect("back");
+});
+
+app.get("/edit", (req, res) => {
+  if (req.query.id) {
+    let editData = data.find((task) => task.id == req.query.id);
+    res.render("edit", { editData: editData });
+  }
+});
+
+app.get("/delete", (req, res) => {
+  data = data.filter((val) => val.id != req.query.id);
+  return res.redirect("back");
+});
+
+app.post("/edit", (req, res) => {
+  let editId = req.body.id;
+  console.log(editId);
+  let EditData = data.filter((val) => {
+    if (val.id == editId) {
+      val.task = req.body.task;
+      val.dsc = req.body.dsc;
+      val.date = req.body.date;
+    }
+    return val;
+  });
+
+  data = EditData;
+  res.redirect("/");
+});
+
+app.get("/show", (req, res) => {
+  return res.render("table", {
+    data: data,
+  });
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Error starting server:", err);
+  } else {
+    console.log(`Server is running on port ${port}`);
+  }
+});
